3c5ed112753e441143f7bf1ab2c8a84c23645255
chenqian <279752917@qq.com>
2020-09-06 09:19:47 +0800

    所有频道接口实现

--- app/resources/article/__init__.py
+++ app/resources/article/__init__.py
@@ -2,6 +2,7 @@ from flask import Blueprint
 from utils.constants import USER_URL_PREFIX
 from flask_restful import Api
 from utils.output import output_json
+from .channel import AllChannelResource
 
 # 1.创建文章蓝图对象
 article_bp = Blueprint("article_bp", __name__, url_prefix=USER_URL_PREFIX)
@@ -13,4 +14,5 @@ article_api = Api(article_bp)
 article_api.representation(mediatype='application/json')(output_json)
 
 # 4.给类视图添加路由信息
-# article_api.add_resource()
+# /app/channels
+article_api.add_resource(AllChannelResource, '/channels')
